a78f75bef9ade5b745177e032ed1a87a2fc4964c
Brian Angulo <brianangulo96@gmail.com>
2021-03-31 23:29:22 -0400

    Player animations work-in-progress

--- scenes.js
+++ scenes.js
@@ -36,8 +36,7 @@ export class TitleScene extends Phaser.Scene {
         bg3.setScale(2, 2);
         bg4.setScale(2, 2);
 
-        //Creates controls object
-        this.controls = this.input.keyboard.createCursorKeys();
+        
     }
 
     update() {
@@ -48,6 +47,7 @@ export class TitleScene extends Phaser.Scene {
 export class GameScene extends Phaser.Scene {
     constructor() {
         super()
+        
     }
     preload() {
         this.load.image("bg0", bg0);
@@ -56,26 +56,59 @@ export class GameScene extends Phaser.Scene {
         this.load.image("bg3", bg3);
         this.load.image("bg4", bg4);
         this.load.spritesheet("player", player, {
-          frameWidth: 42,
-          frameHeight: 48,
+          frameWidth: 47,
+          frameHeight: 50,
         });
     }
-
+    
     create() {
-        const bg0 = this.add.image(400, 250, "bg0");
-        const bg1 = this.add.image(400, 250, "bg1");
-        const bg2 = this.add.image(400, 300, "bg2");
-        const bg3 = this.add.image(400, 300, "bg3");
-        const bg4 = this.add.image(400, 300, "bg4");
-        const player = this.add.sprite(100, 450, "player");
+      //References to game objects
+      const bg0 = this.add.image(400, 250, "bg0");
+      const bg1 = this.add.image(400, 250, "bg1");
+      const bg2 = this.add.image(400, 300, "bg2");
+      const bg3 = this.add.image(400, 300, "bg3");
+      const bg4 = this.add.image(400, 300, "bg4");
+      const player = this.add.sprite(100, 450, "player");
+      //Scale of game objects
+      bg0.setScale(3, 3);
+      bg1.setScale(2, 2);
+      bg2.setScale(2, 2);
+      bg3.setScale(2, 2);
+      bg4.setScale(2, 2);
 
-         bg0.setScale(3, 3);
-         bg1.setScale(2, 2);
-         bg2.setScale(2, 2);
-         bg3.setScale(2, 2);
-         bg4.setScale(2, 2);
+      //Animation
+      this.anims.create({
+        key: "run",
+        frames: this.anims.generateFrameNumbers("player", {
+          frames: [21, 22, 23, 24, 25, 26, 27],
+        }),
+        frameRate: 10,
+        repeat: -1,
+      });
+
+      //Some player controls
+      this.controls = this.input.keyboard.createCursorKeys();
+
+    //   if (this.controls.right.isDown) {
+    //     player.body.setAccelerationX(100);
+    //   }
+
+    
+    //Player misc
+    player.play("run");
+    player.setScale(2, 2);
+    //Add physics to player
+    // this.physics.add.sprite(100, 200, "player");
+    
+    //Moves player gameobject when clicking down
+    this.input.on("pointerdown", function() {
+        player.body.setVelocityX(10);
+    })
     }
 
+    update() {
+        
+    }
 
 }
 
